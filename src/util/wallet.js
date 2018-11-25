9722753f849405986ff052bbb166956cf812cd8c
june <ru-q-ur@163.com>
2018-11-25 14:32:34 +0800

    wallet

--- src/util/wallet.js
+++ src/util/wallet.js
@@ -1,88 +1,115 @@
-// import BIP39 from 'bip39'
-// import HDKey from 'ethereumjs-wallet/hdkey'
-// import Wallet from 'ethereumjs-wallet'
-// import ethUtil from 'ethereumjs-util'
+import BIP39 from 'bip39'
+import HDKey from 'ethereumjs-wallet/hdkey'
+import Wallet from 'ethereumjs-wallet'
+import Util from 'ethereumjs-util'
 
-// // 构造钱包对象
-// function Wallet () {}
+// 生成 mnemonic code
+export function generateMnemonic () {
+  return BIP39.generateMnemonic()
+}
+// 验证 mnemonic code 是否符合BIP39
+export function validateMnemonic (mnemonic) {
+  return BIP39.validateMnemonic(mnemonic)
+}
 
-// // 创建钱包对象
-// function createWallet () {
+// 助记词导入账户
+export class Account {
+  constructor (mnemonic, coinType, accountIndex) {
+    this.mnemonic = mnemonic
+    this.coinType = coinType
+    this.accountIndex = accountIndex
+  }
 
-// }
+  // 助记词生成账户
+  formMnemonic () {
+  // 将 mnemonic code 转成 binary 的 seed
+    const seed = BIP39.mnemonicToSeed(this.mnemonic)
 
-// function fromMasterAccount (mnemonic, wallet, account) {
-//   var ret = new Account()
-//   ret._mnemonic = mnemonic
-//   // 主
-//   ret._wallet = wallet
-//   // 子
-//   ret._account = account
-//   return ret
-// }
+    // 生成 master key
+    // 使用 seed 生成 HD Wallet。如果要说更明确，就是生成 Master Key 并记录起来。
+    const masterAccount = HDKey.fromMasterSeed(seed)
+    // console.log('hdWallet', masterAccount)
+    return masterAccount
+  }
 
-// Account.createAccount = function () {
-//   // 生成 mnemonic code
-//   var mnemonic = BIP39.generateMnemonic()
-//   // 生成 seed
-//   var seed = BIP39.mnemonicToSeed(mnemonic)
+  // 助记词 => seed => masterAccount => derivedNode
+  // 生成子节点
+  generateChildNode () {
+    const masterAccount = this.formMnemonic(this.mnemonic)
 
-//   // 生成 master key
-//   // 使用 seed 生成 HD Wallet。如果要说更明确，就是生成 Master Key 并记录起来。
+    // 生成 Master Private Key
+    // const masterPrvKey = masterAccount.getWallet().getPrivateKeyString()
+    // 生成 Master Public Key
+    // const masterPubKey = masterAccount.getWallet().getPublicKeyString()
 
-//   var _wallet = EthereumHDKey.fromMasterSeed(seed)
-//   // 生成第一个 Ethereum Address
-//   var account = _wallet.derivePath("m/44'/60'/0'/0/0")
+    //   // 生成第一个 Ethereum Address
+    let path = "m/44'/" + this.coinType + "'/0'/0/" + this.accountIndex
 
-//   return fromAccount(mnemonic, _wallet, account.getWallet())
-// }
+    const derivedNode = masterAccount.derivePath(path)
+    // console.log('derivedNode', derivedNode)
 
-// Account.fromAccountByMnemonic = function (mnemonic) {
-//   if (!BIP39.validateMnemonic(mnemonic)) {
-//     throw new Error('Mnemonic is wrong')
-//   }
-//   var seed = BIP39.mnemonicToSeed(mnemonic)
+    return derivedNode
+  }
 
-//   var _wallet = EthereumHDKey.fromMasterSeed(seed)
-//   var account = _wallet.derivePath("m/44'/60'/0'/0/0")
+  get account () {
+    return this.generateChildAccount()
+  }
 
-//   return fromAccount(mnemonic, account.getWallet())
-// }
+  // 子节点生成子账号
+  generateChildAccount () {
+    const childNode = this.generateChildNode(this.accountIndex)
+    const childWallet = childNode.getWallet()
+    const childAccount = getAccountInfo(childWallet)
+    return childAccount
+  }
+}
 
-// Account.fromAccountByPrivateKey = function (privateKey) {
-//   return fromAccount('EMPTY', Wallet.fromPrivateKey(ethUtil.toBuffer(privateKey)))
-// }
+// 用Wallet 得到账号的私钥，公钥，地址
+export function getAccountInfo (account) {
+  // 生成 Private key
+  const prvKey = account.getPrivateKeyString()
+  // 生成 Public key
+  // const childPubKry = derivedNode.getWallet().getPublicKeyString()
+  const rowPubKey = Util.privateToPublic(prvKey)
+  const pubKey = Util.bufferToHex(rowPubKey)
 
-// Account.fromAccountByKeystore = function (keystore, pwd) {
-//   return fromAccount('EMPTY', Wallet.fromV3(keystore, pwd))
-// }
+  //  生成 address。
+  // const childAddr = derivedNode.getWallet().getAddressString()
+  const addr = Util.publicToAddress(rowPubKey)
+  const addrStr = Util.bufferToHex(addr)
 
-// Account.prototype.getMnemonicString = function () {
-//   return this._mnemonic
-// }
+  // 用EIP55 Encoding Address
+  const address = Util.toChecksumAddress(addrStr)
 
-// // 生成 Master Private Key
-// Account.prototype.getMasterPrvKey = function () {
-//   return this._wallet.privateExtendedKey()
-// }
+  return { prvKey, pubKey, address }
+}
 
-// Account.prototype.getAddressString = function () {
-//   return this._wallet.getAddressString()
-// }
+// todo 优化prvKey 取值
+// 私钥恢复钱包
+function prvKey2Wallet (prvKey) {
+  // 去掉前面的0x
+  let rowPrvKey = prvKey.substring(2)
+  var key = Buffer.from(rowPrvKey, 'hex')
+  // 私钥恢复钱包
+  var wallet = Wallet.fromPrivateKey(key)
+  return wallet
+}
 
-// Account.prototype.getPrivateKeyString = function () {
-//   // 生成第一个 Ethereum Address 的 Private key
-//   return this._wallet.getPrivateKeyString()
-// }
+//  加密子节点的 Private key 生成 KeyStore
+export function generateKeyStore (prvKey, password) {
+  var wallet = prvKey2Wallet(prvKey)
+  var keyStore = wallet.toV3String(password)
+  return keyStore
+}
 
-// Account.prototype.getPublicKeyString = function () {
-//   // 生成第一个 Ethereum Address 的 Public key
-//   return this._wallet.getPublicKeyString()
-// }
+// KeyStore导入账户
+//  解密 KeyStore 得到 Private key
+export function fromV3KeyStore (keyStore, password) {
+  return Wallet.fromV3(keyStore, password).getPrivateKeyString()
+}
 
-// Account.prototype.getKeystoreString = function (pwd) {
-//   // 生成第一个 Ethereum Address 的 Keystore
-//   return this._wallet.toV3String(pwd)
-// }
-
-// module.exports = Wallet
+// 私钥导入账户
+export function fromPrivateKey (prvKey) {
+  const wallet = prvKey2Wallet(prvKey)
+  return getAccountInfo(wallet)
+}
--- src/views/createwallet.vue
+++ src/views/createwallet.vue
@@ -7,6 +7,8 @@
 <script>
 import dataForm from '@/components/form'
 
+import { Account, generateMnemonic } from '@/util/wallet'
+
 export default {
   name: 'createwallet',
   components: {
@@ -18,6 +20,10 @@ export default {
 
     }
   },
+  mounted: function () {
+    this.test()
+    // console.log('ss',generateChildAccount('0'))
+  },
   methods: {
     dataForm (dataForm) {
       console.log('dataForm', dataForm)
@@ -26,57 +32,18 @@ export default {
 
       // 异步用 dispatch
       // this.$store.dispatch('createAccountData', dataForm)
-
-      // this.generateAddr()
-    }
-    // generateAddr () {
-    //   let wallet = {}
-
-    //   // 生成 mnemonic code
-    //   wallet.mnemonic = bip39.generateMnemonic()
-
-    //   // 生成 seed
-    //   const seed = bip39.mnemonicToSeed(wallet.mnemonic)
-
-    //   // 生成 master key
-    //   // 使用 seed 生成 HD Wallet。如果要说更明确，就是生成 Master Key 并记录起来。
-    //   const hdWallet = hdkey.fromMasterSeed(seed)
-
-    //   // 生成 Master Private Key
-    //   // const masterPrivateKey = hdWallet._hdkey.privateKey.toString('hex')
-
-    //   // 生成第一个 Ethereum Address
-    //   const derivedNode = hdWallet.derivePath("m/44'/60'/0'/0/0")
-    //   // console.log('derivedNode', derivedNode)
-
-    //   // 生成第一个 Ethereum Address 的 Private key
-    //   wallet.prvKey = derivedNode._hdkey.privateKey.toString('hex')
-
-    //   // 私钥生成公钥
-    //   // 生成第一个 Ethereum Address 的 Public key
-    //   wallet.pubKey = util.privateToPublic(derivedNode._hdkey._privateKey).toString('hex')
-
-    //   // 公钥生成 address。
-    //   wallet.addr = util.pubToAddress(derivedNode._hdkey._publicKey, true)
-
-    //   // 用EIP55 Encoding Address
-    //   wallet.addr = util.toChecksumAddress(wallet.addr.toString('hex'))
-
-    //   this.$store.commit('setWallet', wallet)
-
-    //   let b = derivedNode.getWallet()
-    //   console.log('b', b)
-
-    //   // var walletaaa = Wallet.generate();
-    //   const jsV3 = b.toV3('passwd')
-
-    //   console.log('hdWallet', hdWallet)
-    //   console.log('jsV3', jsV3)
-    //   console.log('address1 EIP55 ', wallet.addr)
+      this.generateAccount()
+    },
+    test () {
+      // 生成 mnemonic code
+      let mnemonic = generateMnemonic()
+      console.log('Account', new Account(mnemonic, '60', '0'))
+    },
+    generateAccount () {
 
     //   // 异步用 dispatch
     //   // this.$store.dispatch('createWalletData', wallet)
-    // }
+    }
   }
 }
 
